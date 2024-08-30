b500ac9d18b94086b07d7f7df7155bc2f67b12cc
Mayank <145135654+ImpactG1@users.noreply.github.com>
2024-08-30 07:39:42 +0530

    Add files via upload

--- NFC3_JSM2-backend1/NFC_project.py
+++ NFC3_JSM2-backend1/NFC_project.py
@@ -79,6 +79,10 @@ ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/map.html')
+def map():
+    return render_template('map.html')
+
 
 @app.route('/add_cat', methods=['GET', 'POST'])
 def add_cat():
---
+++ NFC3_JSM2-backend1/templates/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript is successfully connected!');
+    // Add any additional JavaScript related to the map or other features here
+  });
+  
